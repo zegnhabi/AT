d7fcc6f238fda44a0ee58de4817cf166e02d9ffc
José Ibáñez <zegnhabi@gmail.com>
2019-11-15 16:44:48 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 //creamos la conexion a la basse de datos
-mysqli_connect(getenv('DB_URL'), getenv('DB_USER'), getenv('DB_PASSWORD')) or die(mysql_error());
-mysqli_select_db("at") or die(mysql_error());
+mysqli_connect(getenv('DB_URL'), getenv('DB_USER'), getenv('DB_PASSWORD')) or die(mysqli_error());
+mysqli_select_db("at") or die(mysqli_error());
 ?>
