b4c7118e70079da5c05ecf45dbf94382f1db44d1
José Ibáñez <zegnhabi@gmail.com>
2019-11-19 16:35:23 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
+//add default timezone
+date_default_timezone_set('America/Monterrey');
 //creamos la conexion a la basse de datos
 $link = mysqli_connect(getenv('DB_URL'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME')) or die(mysqli_error());
 mysqli_select_db($link, getenv('DB_NAME')) or die(mysqli_error());
